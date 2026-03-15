437184736b9aa13c5263d5b33eb2facec9d282a0
Jack Zhao <36181514+kcajc@users.noreply.github.com>
2026-03-15 19:20:28 -0400

    fix: suppress vimtex ts warning

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -7,14 +7,8 @@ return {
       vim.g.vimtex_quickfix_mode = 0
       vim.g.vimtex_compiler_method = "latexmk"
       vim.g.vimtex_view_method = "skim"
-
-      -- if vim.fn.executable("sioyek") == 1 then
-        -- vim.g.vimtex_view_method = "sioyek"
-      -- elseif vim.fn.executable("zathura") == 1 then
-        -- vim.g.vimtex_view_method = "zathura"
-      -- elseif vim.fn.has("mac") == 1 and vim.fn.isdirectory("/Applications/Skim.app") == 1 then
-        -- vim.g.vimtex_view_method = "skim"
-      -- end
+      vim.g.vimtex_syntax_enabled = 0
+      vim.g.vimtex_syntax_conceal_disable = 1
     end,
   },
 }
