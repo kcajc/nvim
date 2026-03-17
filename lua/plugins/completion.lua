f4a5acb4cf05125ef4a0f6b71bbb8f34c5f1f3ba
Jack Zhao <36181514+kcajc@users.noreply.github.com>
2026-03-16 21:38:15 -0400

    feat: smart tab completion

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -6,6 +6,25 @@ return {
     opts = {
       keymap = {
         preset = "enter",
+        ["<Tab>"] = {
+          function(cmp)
+            local copilot = require("copilot.suggestion")
+
+            if copilot.is_visible() then
+              copilot.accept()
+              return true
+            end
+
+            if cmp.snippet_active() then
+              return cmp.accept()
+            end
+
+            return cmp.select_and_accept()
+          end,
+          "snippet_forward",
+          "fallback",
+        },
+        ["<S-Tab>"] = { "snippet_backward", "fallback" },
         ["<C-j>"] = { "select_next", "fallback" },
         ["<C-k>"] = { "select_prev", "fallback" },
       },
