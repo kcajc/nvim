9004395681217021402051b72a3e30b29ce2cbf5
Jack Zhao <36181514+kcajc@users.noreply.github.com>
2026-03-20 14:47:09 -0400

    feat: more colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,14 +2,34 @@ return {
   {
     "rose-pine/neovim",
     name = "rose-pine",
+    lazy = false,
     priority = 1000,
     opts = {
-      variant = "auto",
+      variant = "moon",
       dark_variant = "moon",
     },
     config = function(_, opts)
       require("rose-pine").setup(opts)
       vim.cmd.colorscheme("rose-pine")
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 900,
+    opts = {},
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 900,
+    opts = {},
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
     end,
   },
 }
